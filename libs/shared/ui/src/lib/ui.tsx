a2cf6e7074badd8f3e81ea0e346605be6b418479
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-06 12:39:58 +0200

    chore(ui): create ui library

---
+++ libs/shared/ui/src/lib/ui.tsx
@@ -0,0 +1,14 @@
+import * as Styled from './ui.styled';
+
+/* eslint-disable-next-line */
+export interface UiProps {}
+
+export function Ui(props: UiProps) {
+  return (
+    <Styled.StyledUi>
+      <h1>Welcome to Ui!</h1>
+    </Styled.StyledUi>
+  );
+}
+
+export default Ui;
