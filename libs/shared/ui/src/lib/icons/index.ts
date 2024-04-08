203ed582fb319ad1d8138a5e5d7ab48df30ea4cb
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-08 16:44:30 +0200

    chore(ui): add icons SVG file of navbar

---
+++ libs/shared/ui/src/lib/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as CardIcon } from './card-icon.svg';
+export { default as FacebookIcon } from './facebook-icon.svg';
+export { default as InstagramIcon } from './instagram-icon.svg';
+export { default as LocationIcon } from './location-icon.svg';
+export { default as PhoneIcon } from './phone-icon.svg';
+export { default as TwitterIcon } from './twitter-icon.svg';
