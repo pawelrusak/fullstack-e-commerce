27091ff51a3e54d461d92d5c42816ce00a7f2e5c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-09 23:24:15 +0200

    chore(ui): reexport ThemeProvider as client component

--- libs/shared/ui/src/lib/theme/index.ts
+++ libs/shared/ui/src/lib/theme/index.ts
@@ -1,2 +1,5 @@
+'use client';
+
 export { default as theme } from './theme';
 export { default as GlobalStyle } from './global-style';
+export * from './theme-provider';
---
+++ libs/shared/ui/src/lib/theme/theme-provider.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import { ThemeProvider } from 'styled-components';
+
+export { ThemeProvider };
