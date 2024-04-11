d3dc16b47ccfbb2e30b7acd5e1fce0098f310b3b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-11 13:40:18 +0200

    refactor(ui): remove unnecessary initial library component

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,6 +1,5 @@
 'use client';
 
-export * from './lib/ui';
 export * from './lib/theme';
 export * from './lib/brand';
 export * from './lib/navbar';
--- libs/shared/ui/src/lib/ui.styled.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import styled from 'styled-components';
-
-export const StyledUi = styled.div`
-  color: pink;
-  color: ${(props) => props.theme.color.text};
-`;
