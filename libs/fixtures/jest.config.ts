c9215b5937c07bd6f6f1ec870156c34e4914b6bd
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-20 16:02:32 +0200

    chore(fixtures): move fixtures library to correct directory

--- libs/fixtures/jest.config.ts
+++ libs/fixtures/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/lib/fixtures',
+  coverageDirectory: '../../coverage/libs/fixtures',
 };
