9a1ffa43943486efff5efbacdd4b1dc9cb4d71b3
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-21 02:15:17 +0200

    chore(ui): add slick-carousel style to storybook preview

--- libs/shared/ui/.storybook/preview.ts
+++ libs/shared/ui/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import { Preview } from '@storybook/react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { ThemeDecorator, GlobalStyleDecorator } from './decorators';
 
