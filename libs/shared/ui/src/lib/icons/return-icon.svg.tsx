15dab80c1e2e781ceb9943f510696bcfef71c70f
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-27 16:17:26 +0200

    chore(ui): change HTML attributes to JSX attributes in Return Icon svg

--- libs/shared/ui/src/lib/icons/return-icon.svg.tsx
+++ libs/shared/ui/src/lib/icons/return-icon.svg.tsx
@@ -8,7 +8,7 @@ export default function ReturnIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_2_358)">
+      <g clipPath="url(#clip0_2_358)">
         <mask
           id="mask0_2_358"
           style={{ maskType: 'luminance' }}
@@ -24,34 +24,34 @@ export default function ReturnIcon(props: React.SVGProps<SVGSVGElement>) {
           <path
             d="M38.4375 20.3125C38.4375 15.1348 36.3388 10.4473 32.9458 7.05422C29.5526 3.66117 24.8651 1.5625 19.6875 1.5625C14.5098 1.5625 9.82232 3.66117 6.4292 7.05422L7.18748 1.5625"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M1.5625 19.6875C1.5625 24.8652 3.66117 29.5527 7.05422 32.9458C10.4473 36.3388 15.1348 38.4375 20.3125 38.4375C25.4902 38.4375 30.1777 36.3388 33.5708 32.9458L32.8125 38.4375"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M29.375 26.25C29.375 27.9759 27.9759 29.375 26.25 29.375H13.75C12.0241 29.375 10.625 27.9759 10.625 26.25V13.75C10.625 12.0241 12.0241 10.625 13.75 10.625H26.25C27.9759 10.625 29.375 12.0241 29.375 13.75V26.25Z"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M20 16.875V10.625"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </g>
       </g>
