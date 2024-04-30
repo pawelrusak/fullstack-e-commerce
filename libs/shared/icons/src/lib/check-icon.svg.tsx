9fb6a08690b55a466f673349a8f0f8effa766c61
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-04-30 20:50:31 +0200

    chore(icons): add icons to icons library

---
+++ libs/shared/icons/src/lib/check-icon.svg.tsx
@@ -0,0 +1,24 @@
+export default function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_44_2705)">
+        <path
+          d="M23.6486 3.53887C23.1799 3.07021 22.4202 3.07021 21.9515 3.53887L7.57474 17.9158L2.04855 12.3896C1.57995 11.9209 0.820195 11.921 0.351492 12.3896C-0.117164 12.8582 -0.117164 13.6179 0.351492 14.0866L6.72621 20.4612C7.19468 20.9298 7.95499 20.9295 8.42327 20.4612L23.6486 5.23593C24.1172 4.76732 24.1172 4.00753 23.6486 3.53887Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_44_2705">
+          <rect width="24" height="24" fill="transparent" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
