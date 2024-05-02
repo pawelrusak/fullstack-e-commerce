e983b2812b2736a1ef9b8375e24fc4f11d4fdc20
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-05-02 19:08:08 +0200

    chore(frontend): create fetch function for product details

--- apps/frontend/src/app/lib/data.ts
+++ apps/frontend/src/app/lib/data.ts
@@ -13,13 +13,31 @@ export async function fetchFeaturedProducts(): Promise<Product[]> {
       isFeatured: true,
     } satisfies Partial<Product>);
 
-    const dataProducts = await fetch(
+    const data = await fetch(
       `http://localhost:3333/api/v1/products?${queryString}`
     );
 
-    return await dataProducts.json();
+    return await data.json();
   } catch (error) {
     console.error('Server Error:', error);
     throw new Error('Failed to fetch featured products data.');
+  }
+}
+
+export async function fetchProductDetails(
+  slug: Product['slug']
+): Promise<Product> {
+  /**
+   * @todo improve cache management
+   */
+  noStore();
+
+  try {
+    const data = await fetch(`http://localhost:3333/api/v1/products/${slug}`);
+
+    return await data.json();
+  } catch (error) {
+    console.error('Server Error:', error);
+    throw new Error('Failed to fetch product details data.');
   }
 }
