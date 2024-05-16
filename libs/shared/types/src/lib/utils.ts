3acd69d959bbe2c83090d73cf327d9cab25b1451
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-05-16 10:43:35 +0200

    chore(types): create the Paths generic type utils

--- libs/shared/types/src/lib/utils.ts
+++ libs/shared/types/src/lib/utils.ts
@@ -7,3 +7,33 @@ export type Prettify<T> = {
   [K in keyof T]: T[K];
   // eslint-disable-next-line @typescript-eslint/ban-types
 } & {};
+
+/**
+ * @see {@link https://stackoverflow.com/questions/58434389/typescript-deep-keyof-of-a-nested-object}
+ */
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never;
+
+/**
+ * @see {@link https://stackoverflow.com/questions/58434389/typescript-deep-keyof-of-a-nested-object}
+ */
+// prettier-ignore
+type Prev = [
+  never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]
+];
+
+/**
+ * @see {@link https://stackoverflow.com/questions/58434389/typescript-deep-keyof-of-a-nested-object}
+ */
+export type Paths<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? {
+      [K in keyof T]-?: K extends string | number
+        ? `${K}` | Join<K, Paths<T[K], Prev[D]>>
+        : never;
+    }[keyof T]
+  : '';
