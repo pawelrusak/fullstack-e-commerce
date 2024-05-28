f1f392ec49956f664415a89226389bdac6830b19
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-05-28 19:53:35 +0200

    style(features): add white spaces to tests cases

--- libs/shared/factories/src/specs/sub-category.factory.spec.ts
+++ libs/shared/factories/src/specs/sub-category.factory.spec.ts
@@ -3,6 +3,7 @@ import { subCategoryFactory } from '../lib/sub-category.factory';
 describe('subCategoryFactory', () => {
   it('should return a sub category with specified shape', () => {
     const subCategory = subCategoryFactory();
+
     expect(subCategory).toEqual({
       _id: expect.any(String),
       name: expect.any(String),
