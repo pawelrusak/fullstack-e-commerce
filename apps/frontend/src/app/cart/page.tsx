ab5c83f132a5ddedeb849c1e944e82994fcf6d60
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-06-07 21:25:37 +0200

    chore(frontend): create base cart page

---
+++ apps/frontend/src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+import { Heading, LayoutWrapper } from '@e-shop/ui';
+
+export default function CartPage() {
+  return (
+    <main>
+      <LayoutWrapper>
+        <Heading level={1} as="h2">
+          Your Cart
+        </Heading>
+      </LayoutWrapper>
+    </main>
+  );
+}
