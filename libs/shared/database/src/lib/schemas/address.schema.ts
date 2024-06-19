b06f34ec381de8afdde2b0850d744a98d8fb6b54
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-06-19 22:49:33 +0200

    chore(database): create address schema

---
+++ libs/shared/database/src/lib/schemas/address.schema.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+import type { AddressSchema } from '@e-shop/types';
+
+export const addressSchema = new mongoose.Schema<AddressSchema>({
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
