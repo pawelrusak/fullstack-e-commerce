a915b702f66aff254952eee4ba6a27dceaf871f0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-03 15:48:30 +0200

    chore(validations): improve the address schema

--- libs/shared/validations/src/lib/schemas/address.schema.ts
+++ libs/shared/validations/src/lib/schemas/address.schema.ts
@@ -1,10 +1,11 @@
 import { z, ZodType } from 'zod';
 import { Address } from '@e-shop/types';
 
+// TODO improve schema (change min and max length etc.)
 export const addressSchema: ZodType<Address> = z.object({
-  address: z.string(),
-  country: z.string(),
-  state: z.string(),
-  city: z.string(),
-  postalCode: z.string(),
+  address: z.string().min(1).trim(),
+  country: z.string().min(1).trim(),
+  state: z.string().min(1).trim(),
+  city: z.string().min(1).trim(),
+  postalCode: z.string().min(1).trim(),
 });
