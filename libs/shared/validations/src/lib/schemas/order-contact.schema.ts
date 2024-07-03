05b80beda54727e21dedc4557984f003f81d3b5d
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-03 15:47:05 +0200

    chore(validations): improve the order contact schema

--- libs/shared/validations/src/lib/schemas/order-contact.schema.ts
+++ libs/shared/validations/src/lib/schemas/order-contact.schema.ts
@@ -4,6 +4,7 @@ import { OrderContact } from '@e-shop/types';
 export const orderContactSchema: ZodType<OrderContact> = z.object({
   firstName: z.string().min(2).max(64).trim(),
   lastName: z.string().min(2).max(64).trim(),
-  phone: z.string().trim(),
+  // TODO improve phone validation (change min and max length etc.)
+  phone: z.string().trim().min(1),
   email: z.string().email().max(8).max(256).trim(),
 });
