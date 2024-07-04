3f9d5c224d0db9b38092116d370ae16a3b96438b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-04 22:47:18 +0200

    chore(database): export the Order model

--- libs/shared/database/src/models.ts
+++ libs/shared/database/src/models.ts
@@ -1,3 +1,4 @@
 export { SubCategory } from './lib/models/subCategory.model';
 export { Category } from './lib/models/category.model';
 export { Product } from './lib/models/product';
+export { Order } from './lib/models/order.model';
