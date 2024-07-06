e5609bfd7224727492baad10d55fb0d50bd73f9e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-06 22:27:27 +0200

    chore(frontend): add form validation to BillingDetailsForm component

--- apps/frontend/src/app/components/billing-details-form.component.tsx
+++ apps/frontend/src/app/components/billing-details-form.component.tsx
@@ -1,10 +1,16 @@
 'use client';
 
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { EN } from '@e-shop/i18n';
 import { Field, Panel, Heading, SimpleCartList, Button } from '@e-shop/ui';
 import { getCurrencyFormat } from '@e-shop/utils';
 import { withStore, useHydrationCartStore } from '@e-shop/store';
+import {
+  CheckoutInformationFrom,
+  checkoutInformationFormSchema,
+} from '../lib/validations';
 
 const Form = styled.form`
   display: grid;
@@ -80,14 +86,27 @@ const SummaryBoxValue = styled.div`
 function BillingDetailsForm() {
   const { cartStore, isHydrated } = useHydrationCartStore();
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<CheckoutInformationFrom>({
+    resolver: zodResolver(checkoutInformationFormSchema),
+  });
+
+  const handleCheckoutFormSubmit = (data: CheckoutInformationFrom) => {
+    console.log(data);
+    console.error('@@@ error: ', errors);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(handleCheckoutFormSubmit)}>
       <div>
         <FormTitle as="h2" level={2}>
           {EN.CHECKOUT.INFORMATION.FORM.TITLE}
         </FormTitle>
         <InformationFields>
-          <Field>
+          <Field valid={!errors.firstName}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.FIRST_NAME.LABEL}
             </Field.Label>
@@ -96,10 +115,12 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.FIRST_NAME.PLACEHOLDER
               }
               type="text"
+              {...register('firstName')}
             />
+            <Field.Error>{errors.firstName?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.lastName}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.LAST_NAME.LABEL}
             </Field.Label>
@@ -108,11 +129,14 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.LAST_NAME.PLACEHOLDER
               }
               type="text"
+              {...register('lastName')}
             />
+            <Field.Error>{errors.lastName?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.phone}>
             <Field.Label isRequired>
+              {/* TODO rename to FIELD.PHONE.LABEL */}
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.PHONE_NUMBER.LABEL}
             </Field.Label>
             <Field.ControlInput
@@ -120,10 +144,12 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.PHONE_NUMBER.PLACEHOLDER
               }
               type="text"
+              {...register('phone')}
             />
+            <Field.Error>{errors.phone?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.email}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.EMAIL_ADDRESS.LABEL}
             </Field.Label>
@@ -132,10 +158,12 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.EMAIL_ADDRESS.PLACEHOLDER
               }
               type="email"
+              {...register('email')}
             />
+            <Field.Error>{errors.email?.message}</Field.Error>
           </Field>
 
-          <WideField>
+          <WideField valid={!errors.address}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.ADDRESS.LABEL}
             </Field.Label>
@@ -144,10 +172,12 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.ADDRESS.PLACEHOLDER
               }
               type="text"
+              {...register('address')}
             />
+            <Field.Error>{errors.address?.message}</Field.Error>
           </WideField>
 
-          <Field>
+          <Field valid={!errors.country}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.COUNTRY.LABEL}
             </Field.Label>
@@ -156,30 +186,36 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.COUNTRY.PLACEHOLDER
               }
               type="text"
+              {...register('country')}
             />
+            <Field.Error>{errors.country?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.state}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.STATE.LABEL}
             </Field.Label>
             <Field.ControlInput
               placeholder={EN.CHECKOUT.INFORMATION.FORM.FIELD.STATE.PLACEHOLDER}
               type="text"
+              {...register('state')}
             />
+            <Field.Error>{errors.state?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.city}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.CITY.LABEL}
             </Field.Label>
             <Field.ControlInput
               placeholder={EN.CHECKOUT.INFORMATION.FORM.FIELD.CITY.PLACEHOLDER}
               type="text"
+              {...register('city')}
             />
+            <Field.Error>{errors.city?.message}</Field.Error>
           </Field>
 
-          <Field>
+          <Field valid={!errors.postalCode}>
             <Field.Label isRequired>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.POSTAL_CODE.LABEL}
             </Field.Label>
@@ -188,10 +224,12 @@ function BillingDetailsForm() {
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.POSTAL_CODE.PLACEHOLDER
               }
               type="text"
+              {...register('postalCode')}
             />
+            <Field.Error>{errors.postalCode?.message}</Field.Error>
           </Field>
 
-          <WideField mb={0}>
+          <WideField mb={0} valid={!errors.customerNote}>
             <Field.Label>
               {EN.CHECKOUT.INFORMATION.FORM.FIELD.ORDER_NOTES.LABEL}
             </Field.Label>
@@ -201,7 +239,10 @@ function BillingDetailsForm() {
               placeholder={
                 EN.CHECKOUT.INFORMATION.FORM.FIELD.ORDER_NOTES.PLACEHOLDER
               }
+              // TODO rename to order Notes, or similar
+              {...register('customerNote')}
             />
+            <Field.Error>{errors.customerNote?.message}</Field.Error>
           </WideField>
         </InformationFields>
       </div>
@@ -233,7 +274,9 @@ function BillingDetailsForm() {
           <SummaryBoxInner>
             <SummaryBoxKey>{EN.CHECKOUT.PANEL.SUBTOTAL}</SummaryBoxKey>
             <SummaryBoxValue>
-              {getCurrencyFormat(cartStore.totalItemsPrice)}
+              {isHydrated
+                ? getCurrencyFormat(cartStore.totalItemsPrice)
+                : 'Loading...'}
             </SummaryBoxValue>
           </SummaryBoxInner>
           <Button type="submit" fullWidth>
