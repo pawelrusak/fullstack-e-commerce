93f3030bf65b999a83cd6ec38b1f1c86f48deba9
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-06 22:29:43 +0200

    feat(frontend): create the Checkout Information page

---
+++ apps/frontend/src/app/checkout/information/page.tsx
@@ -0,0 +1,10 @@
+import { LayoutWrapper } from '@e-shop/ui';
+import BillingDetailsForm from '../../components/billing-details-form.component';
+
+export default function CheckoutInformationPage() {
+  return (
+    <LayoutWrapper>
+      <BillingDetailsForm />
+    </LayoutWrapper>
+  );
+}
