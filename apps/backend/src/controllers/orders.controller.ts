c6eaec84169e352f222cac721485d7f7588ee01b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-11 19:22:29 +0200

    feat(backend): register new order in POST /orders/register endpoint

--- apps/backend/src/controllers/orders.controller.ts
+++ apps/backend/src/controllers/orders.controller.ts
@@ -1,5 +1,7 @@
 import { Controller } from '@e-shop/types';
+import { Order } from '@e-shop/database/models';
 import { STATUS_CODE } from '@e-shop/utils';
+import { OrderPostRequestBody } from '@e-shop/types/request';
 
 export default {
   /**
@@ -9,17 +11,26 @@ export default {
    * @access Public
    */
   async register(request, response) {
+    const { body }: { body: OrderPostRequestBody } = request;
     // TODO use transactions
 
     // TODO validate product prices and maximum quantity of the order.
 
     // TODO validate that product exist in database.
 
-    // TODO register (create) a new order
+    await Order.register({
+      // TODO add this property when register featured will be added
+      customer: undefined,
+      contact: body.contact,
+      products: body.products,
+      shippingAddress: body.shippingAddress,
+      paymentMethod: body.paymentMethod,
+      customerNote: body.customerNote,
+    });
 
     // TODO update product stocks
 
-    response.status(STATUS_CODE.NOT_IMPLEMENTED).send({
+    response.status(STATUS_CODE.CREATED).send({
       message: `This action created a new order and update product stock`,
     });
   },
