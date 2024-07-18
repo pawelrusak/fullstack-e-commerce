3028570ebed4f98969ce880449af485be83317b2
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-18 19:20:05 +0200

    chore(database): improve variables names in getInvalidObjectIds function

--- libs/shared/database/src/lib/utils/get-invalid-object-ids.ts
+++ libs/shared/database/src/lib/utils/get-invalid-object-ids.ts
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 type ObjectId = Parameters<typeof mongoose.isValidObjectId>[0];
 
 export function getInvalidObjectIds(...objectIds: ObjectId[]) {
-  return objectIds.filter((productId) => !mongoose.isValidObjectId(productId));
+  return objectIds.filter((objectId) => !mongoose.isValidObjectId(objectId));
 }
