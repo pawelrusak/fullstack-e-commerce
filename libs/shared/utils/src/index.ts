65585bacab68832bf2285cb7a7555a5d129f9e0c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-20 19:09:02 +0200

    chore(utils): create RegisterOrderError error

--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -1,9 +1,9 @@
 export * from './lib/utils';
 export { STATUS_CODE } from './lib/status-code';
-export { InvalidObjectIdsError } from './lib/errors';
 export { getValueInRange } from './lib/get-value-in-range';
 export { useHydrated } from './lib/hooks/use-hydrated.hook';
 export { getCurrencyFormat } from './lib/get-currency-format';
 export { getOrderProductsIds } from './lib/get-order-products-ids';
 export { replaceByCurrentYear } from './lib/replace-by-current-year';
 export { getCategoryQueryString } from './lib/get-category-query-string';
+export { InvalidObjectIdsError, RegisterOrderError } from './lib/errors';
--- libs/shared/utils/src/lib/errors.ts
+++ libs/shared/utils/src/lib/errors.ts
@@ -1,3 +1,7 @@
 export class InvalidObjectIdsError extends Error {
   override name = 'InvalidObjectIdsError';
 }
+
+export class RegisterOrderError extends Error {
+  override name = 'RegisterOrderError';
+}
