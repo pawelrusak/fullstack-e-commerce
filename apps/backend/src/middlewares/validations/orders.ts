409213d8a657c23404301c506f0bb6792adf218d
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-21 14:06:09 +0200

    chore(backend): add assertions for product prices and stock availability

--- apps/backend/src/middlewares/validations/orders.ts
+++ apps/backend/src/middlewares/validations/orders.ts
@@ -2,11 +2,12 @@ import {
   getOrderProductsIds,
   InvalidObjectIdsError,
   RegisterOrderError,
+  getCurrencyFormat,
 } from '@e-shop/utils';
 import { isValidObjectIds, getInvalidObjectIds } from '@e-shop/database/utils';
 import { Product } from '@e-shop/database/models';
 
-import { Order } from '@e-shop/types';
+import type { Order, OrderProduct } from '@e-shop/types';
 import type { Request, Response, NextFunction } from 'express';
 import type { OrderPostRequestBody } from '@e-shop/types/request';
 
@@ -37,6 +38,44 @@ async function assertProductsInDatabaseOrFail(productsIds: string[]) {
   return true;
 }
 
+async function assertProductsIsNotEnoughAndPriceNotMatches(
+  orderProducts: OrderProduct[],
+) {
+  const orderProductsId = getOrderProductsIds(orderProducts);
+
+  const productsDB = await Product.find({ _id: { $in: orderProductsId } });
+
+  productsDB.forEach((productBD) => {
+    const productDBId = String(productBD._id);
+    const productDBPrice = productBD.price;
+    const productBDstock = productBD.stock;
+
+    const orderProduct = orderProducts.find(
+      (orderProduct) => orderProduct.product._id === productDBId,
+    );
+
+    if (orderProduct.currentProductPrice !== productDBPrice) {
+      throw new RegisterOrderError(
+        `Price mismatch for product ID ${productDBId}: The provided price does not match the price in the database. Your product price is ${getCurrencyFormat(
+          orderProduct.currentProductPrice,
+        )}, but this product costs ${getCurrencyFormat(productDBPrice)}`,
+      );
+    }
+
+    if (orderProduct.quantity <= 0) {
+      throw new RegisterOrderError(
+        `Incorrect quantity in order for product with ID ${productDBId}, your order has ${orderProduct.quantity} products`,
+      );
+    }
+
+    if (productBDstock <= orderProduct.quantity) {
+      throw new RegisterOrderError(
+        `Not enough product with ID ${productDBId}, there is only ${productBDstock} of this product in the stock, but you order ${orderProduct.quantity}`,
+      );
+    }
+  });
+}
+
 type ParamsDictionary = Record<string, string>;
 
 export async function validateOrderProductsPriceAndStock(
@@ -64,9 +103,8 @@ export async function validateOrderProductsPriceAndStock(
   // Validate that products exists in database
   await assertProductsInDatabaseOrFail(orderProductsId);
 
-  // TODO validate that price is this same as in database
-
-  // TODO validate that whether more products have been purchased than are in the database
+  // Validate that the price matches the database and ensure the purchased quantity does not exceed available stock
+  await assertProductsIsNotEnoughAndPriceNotMatches(orderBody.products);
 
   next();
 }
