6f12625a1327ef2f03aa866c2ff793574ed33a07
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-21 14:52:05 +0200

    chore(utils): add errors code to status codes object

--- libs/shared/utils/src/specs/status-code.spec.ts
+++ libs/shared/utils/src/specs/status-code.spec.ts
@@ -8,9 +8,13 @@ describe('STATUS_CODE', () => {
   it('should have a specific shape', () => {
     expect(STATUS_CODE).toMatchInlineSnapshot(`
       {
+        "BAD_REQUEST": 400,
         "CREATED": 201,
+        "INTERNAL_SERVER_ERROR": 500,
+        "NOT_FOUND": 404,
         "NOT_IMPLEMENTED": 501,
         "OK": 200,
+        "UNPROCESSABLE_CONTENT": 422,
       }
     `);
   });
