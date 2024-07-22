57ee828f95083fede9193c3961a399dd04e67cf2
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-22 16:05:40 +0200

    feat(backend): add database transaction for registering a new order

--- apps/backend/src/controllers/orders.controller.ts
+++ apps/backend/src/controllers/orders.controller.ts
@@ -1,5 +1,6 @@
 import { STATUS_CODE } from '@e-shop/utils';
 import { Order, Product } from '@e-shop/database/models';
+import { runInTransaction } from '@e-shop/database/utils';
 
 import type { Response } from 'express';
 import type { Controller } from '@e-shop/types';
@@ -11,36 +12,38 @@ export default {
    * This action registers a new order before payment is made
    *
    * @route POST /api/v1/orders/register
+   * @request {@link ../middlewares/validations/orders.ts}
    * @access Public/Private
    */
   async register(request, response: Response<CreateResponseBody>) {
-    const { body: orderBody }: { body: OrderPostRequestBody } = request;
-    // TODO use transactions
+    await runInTransaction(async (session) => {
+      const { body: orderBody }: { body: OrderPostRequestBody } = request;
+      // TODO use transactions
 
-    // TODO validate product prices and maximum quantity of the order.
+      const order = await Order.register(
+        {
+          // TODO add this property when register featured will be added
+          customer: undefined,
+          contact: orderBody.contact,
+          products: orderBody.products,
+          shippingAddress: orderBody.shippingAddress,
+          paymentMethod: orderBody.paymentMethod,
+          customerNote: orderBody.customerNote,
+        },
+        { session },
+      );
 
-    // TODO validate that product exist in database.
+      await Product.updateStockByOrder(orderBody, { session });
 
-    const order = await Order.register({
-      // TODO add this property when register featured will be added
-      customer: undefined,
-      contact: orderBody.contact,
-      products: orderBody.products,
-      shippingAddress: orderBody.shippingAddress,
-      paymentMethod: orderBody.paymentMethod,
-      customerNote: orderBody.customerNote,
-    });
+      // TODO (in future) send e-mail do client
 
-    await Product.updateStockByOrder(orderBody);
-
-    // TODO (in future) send e-mail do client
-
-    response.status(STATUS_CODE.CREATED).send({
-      message: `This action created a new order and update products stocks`,
-      status: STATUS_CODE.CREATED,
-      data: {
-        order,
-      },
+      response.status(STATUS_CODE.CREATED).send({
+        message: `This action created a new order and update products stocks`,
+        status: STATUS_CODE.CREATED,
+        data: {
+          order,
+        },
+      });
     });
   },
 } satisfies Controller;
