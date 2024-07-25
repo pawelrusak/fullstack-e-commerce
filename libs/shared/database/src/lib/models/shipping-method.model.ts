aef066535ae15fbc9720cd90f51a49e16179d1f5
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-25 18:00:56 +0200

    chore(database): create ShippingMethod model

---
+++ libs/shared/database/src/lib/models/shipping-method.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+import type { ShippingMethodSchema } from '@e-shop/types';
+
+export const shippingMethodSchema = new mongoose.Schema<ShippingMethodSchema>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  cost: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  image: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+});
+
+export const ShippingMethod = mongoose.model<ShippingMethodSchema>(
+  'ShippingMethod',
+  shippingMethodSchema,
+);
--- libs/shared/database/src/models.ts
+++ libs/shared/database/src/models.ts
@@ -1,3 +1,4 @@
+export { ShippingMethod } from './lib/models/shipping-method.model';
 export { SubCategory } from './lib/models/subCategory.model';
 export { Category } from './lib/models/category.model';
 export { User } from './lib/models/user.model';
