fe75d67d1c70e8c880425b6feb4fa507b7d05856
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-27 18:51:42 +0200

    fix(database): add missing timestamp fields to ShippingMethod model

--- libs/shared/database/src/lib/models/shipping-method.model.ts
+++ libs/shared/database/src/lib/models/shipping-method.model.ts
@@ -2,24 +2,29 @@ import mongoose from 'mongoose';
 
 import type { ShippingMethodSchema } from '@e-shop/types';
 
-export const shippingMethodSchema = new mongoose.Schema<ShippingMethodSchema>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+export const shippingMethodSchema = new mongoose.Schema<ShippingMethodSchema>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    cost: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+      trim: true,
+    },
   },
-  cost: {
-    type: Number,
-    required: true,
-    min: 0,
+  {
+    timestamps: true,
   },
-  image: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-});
+);
 
 export const ShippingMethod = mongoose.model<ShippingMethodSchema>(
   'ShippingMethod',
