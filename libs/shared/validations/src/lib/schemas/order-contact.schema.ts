4130188406d466ec6fe36eb283ae0f98863723c5
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-07-29 21:40:20 +0200

    fix(validations): change email min length in order contact schema

--- libs/shared/validations/src/lib/schemas/order-contact.schema.ts
+++ libs/shared/validations/src/lib/schemas/order-contact.schema.ts
@@ -5,5 +5,5 @@ export const orderContactSchema = z.object({
   lastName: z.string().min(2).max(64).trim(),
   // TODO improve phone validation (change min and max length etc.)
   phone: z.string().trim().min(1),
-  email: z.string().email().max(8).max(256).trim(),
+  email: z.string().email().min(8).max(256).trim(),
 });
