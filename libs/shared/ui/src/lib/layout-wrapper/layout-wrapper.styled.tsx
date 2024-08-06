0dcc842e62dd43be0c3375ce05f2970e457c6527
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-08-06 19:40:20 +0200

    fix(ui): reduce the maximum width of layout wrapper component

--- libs/shared/ui/src/lib/layout-wrapper/layout-wrapper.styled.tsx
+++ libs/shared/ui/src/lib/layout-wrapper/layout-wrapper.styled.tsx
@@ -7,7 +7,7 @@ export const layoutWrapperCss = css`
   padding-right: 1rem;
 
   @media screen and (min-width: ${({ theme }) => theme.screens.sm}) {
-    max-width: 158rem;
+    max-width: 156rem;
     padding-left: 2rem;
     padding-right: 2rem;
     margin: 0 auto;
