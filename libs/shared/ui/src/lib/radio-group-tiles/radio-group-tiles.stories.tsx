86fa5058fec56bc257a5ada3020b5aa57d13cc36
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-08-14 22:39:47 +0200

    chore(ui): add stories for invalid state of Radio Group Tiles component

--- libs/shared/ui/src/lib/radio-group-tiles/radio-group-tiles.stories.tsx
+++ libs/shared/ui/src/lib/radio-group-tiles/radio-group-tiles.stories.tsx
@@ -28,3 +28,23 @@ type Story = StoryObj<typeof RadioGroupTiles>;
 export const Complete: Story = {
   args: {},
 };
+
+export const Invalid: Story = {
+  args: {
+    valid: false,
+  },
+  render: (args) => (
+    <RadioGroupTiles radioGroupName="shipping" required {...args}>
+      {storyShippingMethods.map((method) => (
+        <RadioGroupTiles.Tile key={method._id}>
+          <RadioGroupTiles.TileRadioInput value={method._id} />
+          <RadioGroupTiles.TileLabel>{method.name}</RadioGroupTiles.TileLabel>
+          <RadioGroupTiles.TileImage src={method.image} />
+          <RadioGroupTiles.TileAdditionalText>
+            {getCurrencyFormat(method.cost)}
+          </RadioGroupTiles.TileAdditionalText>
+        </RadioGroupTiles.Tile>
+      ))}
+    </RadioGroupTiles>
+  ),
+};
