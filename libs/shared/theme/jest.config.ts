5ec2461f63bf1d742bf7b9ea3a29a90c074cf3c0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-08-19 10:01:18 +0200

    test(theme): add jest config files

---
+++ libs/shared/theme/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+/* eslint-disable */
+export default {
+  displayName: 'theme',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/shared/theme',
+} satisfies Config;
