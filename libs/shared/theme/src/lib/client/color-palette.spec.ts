4049a11a748a60d1fe97ee50f1100ae0c6489634
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-08-24 12:50:20 +0200

    chore(theme): add secondary color to color palette

--- libs/shared/theme/src/lib/client/color-palette.spec.ts
+++ libs/shared/theme/src/lib/client/color-palette.spec.ts
@@ -16,6 +16,19 @@ describe('Color palette', () => {
         900: expect.any(String),
         950: expect.any(String),
       },
+      secondary: {
+        50: expect.any(String),
+        100: expect.any(String),
+        200: expect.any(String),
+        300: expect.any(String),
+        400: expect.any(String),
+        500: expect.any(String),
+        600: expect.any(String),
+        700: expect.any(String),
+        800: expect.any(String),
+        900: expect.any(String),
+        950: expect.any(String),
+      },
     });
   });
 
@@ -34,6 +47,19 @@ describe('Color palette', () => {
           "800": "hsl(7, 83%, 35%)",
           "900": "hsl(7, 74%, 31%)",
           "950": "hsl(7, 90%, 15%)",
+        },
+        "secondary": {
+          "100": "hsl(0, 0%, 62%)",
+          "200": "hsl(0, 0%, 51%)",
+          "300": "hsl(0, 0%, 40%)",
+          "400": "hsl(0, 0%, 30%)",
+          "50": "hsl(0, 0%, 73%)",
+          "500": "hsl(0, 0%, 19%)",
+          "600": "hsl(0, 0%, 16%)",
+          "700": "hsl(0, 0%, 14%)",
+          "800": "hsl(0, 0%, 12%)",
+          "900": "hsl(0, 0%, 9%)",
+          "950": "hsl(0, 0%, 6%)",
         },
       }
     `);
