908a44b76b38633fdf0d8401f903ceb954a90f75
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-08-26 22:13:15 +0200

    chore(theme): export design token utilities

--- libs/shared/theme/src/lib/client/index.ts
+++ libs/shared/theme/src/lib/client/index.ts
@@ -3,6 +3,7 @@
 export { default as theme } from './theme';
 export { colorPalette } from './color-palette';
 export { default as GlobalStyle } from './global-style';
+export { getComponentThemeToken, setupGetThemeToken } from './token.utils';
 export { AppThemeProvider } from './app-theme-provider';
 
 export type { AppThemeProviderProps } from './app-theme-provider';
