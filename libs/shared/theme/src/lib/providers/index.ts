81a0a6f59a1ec30acff238d5dbad31ba3b24d7cf
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-05 19:36:51 +0200

    chore(theme): update import in theme library subdirectory

--- libs/shared/theme/src/lib/providers/index.ts
+++ libs/shared/theme/src/lib/providers/index.ts
@@ -1,6 +1,6 @@
 'use client';
 
-export { default as GlobalStyle } from '../providers/global-style';
+export { default as GlobalStyle } from './global-style';
 export { AppThemeProvider } from './app-theme-provider';
 
 export type { AppThemeProviderProps } from './app-theme-provider';
