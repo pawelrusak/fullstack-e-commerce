96421f5dc9bb8569807ec87871344671d56c975b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-08 22:40:59 +0200

    chore(types): add utility type to extract values from object

--- libs/shared/types/src/lib/utils.ts
+++ libs/shared/types/src/lib/utils.ts
@@ -31,9 +31,11 @@ type Prev = [
 export type Paths<T, D extends number = 10> = [D] extends [never]
   ? never
   : T extends object
-  ? {
-      [K in keyof T]-?: K extends string | number
-        ? `${K}` | Join<K, Paths<T[K], Prev[D]>>
-        : never;
-    }[keyof T]
-  : '';
+    ? {
+        [K in keyof T]-?: K extends string | number
+          ? `${K}` | Join<K, Paths<T[K], Prev[D]>>
+          : never;
+      }[keyof T]
+    : '';
+
+export type ObjectValues<T> = T[keyof T];
