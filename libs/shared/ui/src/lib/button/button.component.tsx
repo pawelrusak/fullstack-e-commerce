a0d934be572185ec888632afc1f8e450942733f8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-09 21:18:24 +0200

    chore(ui): export related types and constants for button component

--- libs/shared/ui/src/lib/button/button.component.tsx
+++ libs/shared/ui/src/lib/button/button.component.tsx
@@ -1,5 +1,20 @@
+import { StyledComponent, DefaultTheme } from 'styled-components';
 import * as Styled from './button.styled';
+import { BUTTON_VARIANT, BUTTON_COLOR_VARIANT } from './button.styled';
+import type {
+  StyledButtonProps,
+  ButtonColorVariant,
+  ButtonVariant,
+} from './button.styled';
+
+export type ButtonProps = StyledComponent<
+  'button',
+  DefaultTheme,
+  StyledButtonProps
+>;
 
 export const Button = Styled.Button;
 
 export default Button;
+export { BUTTON_VARIANT, BUTTON_COLOR_VARIANT };
+export type { ButtonColorVariant, ButtonVariant };
