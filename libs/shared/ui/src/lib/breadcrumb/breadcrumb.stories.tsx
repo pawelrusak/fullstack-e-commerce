7ca738d1e0276000591122c738767e2ba58b4933
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-12 11:06:24 +0200

    chore(ui): replace i18n strings with static text in breadcrumb stories

--- libs/shared/ui/src/lib/breadcrumb/breadcrumb.stories.tsx
+++ libs/shared/ui/src/lib/breadcrumb/breadcrumb.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Breadcrumb } from './breadcrumb.component';
-import { EN } from '@e-shop/i18n';
 
 export default {
   component: Breadcrumb,
   title: 'Breadcrumb',
   render: () => (
     <Breadcrumb>
-      <Breadcrumb.Item href="#">{EN.BREADCRUMB.HOME}</Breadcrumb.Item>
+      <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
       <Breadcrumb.Item href="#">Computers & Tablets</Breadcrumb.Item>
       <Breadcrumb.Item href="#">Laptop</Breadcrumb.Item>
       <Breadcrumb.Item href="#" isCurrent>
