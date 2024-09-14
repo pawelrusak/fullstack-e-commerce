29bcbbc87a2c490b7b5fbaf1bfce0651ffcce8d8
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-14 11:34:40 +0200

    refactor(ui): expand border-top shorthand in Divider component

--- libs/shared/ui/src/lib/divider/divider.styled.tsx
+++ libs/shared/ui/src/lib/divider/divider.styled.tsx
@@ -3,10 +3,12 @@
 import styled from 'styled-components';
 import * as helper from 'polished';
 
-// @todo add margin from styled-system
+// TODO: add margin from styled-system
 export const Divider = styled.hr`
   border: none;
-  border-top: 1px solid
-    ${({ theme }) => helper.transparentize(0.75, theme.color.text)};
+  border-top-width: 1px;
+  border-top-style: solid;
+  border-top-color: ${({ theme }) =>
+    helper.transparentize(0.75, theme.color.text)};
   margin: 1.6rem 0;
 `;
