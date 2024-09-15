222ed9dc0275f4de09e13f97ba352218a7a38c86
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-15 14:48:18 +0200

    feat(ui): add margin props to Divider component

--- libs/shared/ui/src/lib/divider/divider.styled.tsx
+++ libs/shared/ui/src/lib/divider/divider.styled.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import styled from 'styled-components';
+// TODO: Use adapter pattern for styled-system library
+import { margin } from 'styled-system';
 import { getComponentThemeToken as getToken } from '@e-shop/theme';
 
-// TODO: add margin from styled-system
-export const Divider = styled.hr`
+import type { MarginProps } from 'styled-system';
+
+export const Divider = styled.hr<MarginProps>`
   border: none;
   border-top-width: 1px;
   border-top-style: solid;
   border-top-color: ${getToken('divider.base.palette.borderColor')};
   margin: 1.6rem 0;
+  ${margin}
 `;
