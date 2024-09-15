58304accc7348b01b5c7fa2d46b65eff120edde1
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-15 17:13:45 +0200

    chore(ui): add margin argTypes to Divider component stories

--- libs/shared/ui/src/lib/divider/divider.stories.tsx
+++ libs/shared/ui/src/lib/divider/divider.stories.tsx
@@ -4,6 +4,36 @@ import { Divider } from './divider.component';
 export default {
   component: Divider,
   title: 'Divider',
+  argTypes: {
+    mb: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    mt: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    mr: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    ml: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    m: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    mx: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    my: {
+      control: 'select',
+      options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+  },
 } satisfies Meta<typeof Divider>;
 
 type Story = StoryObj<typeof Divider>;
