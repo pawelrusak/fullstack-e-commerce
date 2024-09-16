0dc2e553b9f1ef7abc33a1c847ee4a9937d86c84
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-16 16:39:52 +0200

    chore(ui): create spacing highlight decorator for Storybook

--- libs/shared/ui/.storybook/decorators.tsx
+++ libs/shared/ui/.storybook/decorators.tsx
@@ -1,3 +1,4 @@
+import styled, { ThemeProvider } from 'styled-components';
 import { GlobalStyle, AppThemeProvider, theme } from '@e-shop/theme';
 import { Decorator } from '@storybook/react';
 
@@ -15,5 +16,21 @@ export const AppThemeDecorator: Decorator = (Story) => {
     <AppThemeProvider>
       <Story />
     </AppThemeProvider>
+  );
+};
+
+const SpacingHighlightContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.primaryContainer};
+  border: 1px solid transparent;
+  margin-bottom: 12px;
+`;
+
+export const SpacingHighlightDecorator: Decorator = (Story) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <SpacingHighlightContainer>
+        <Story />
+      </SpacingHighlightContainer>
+    </ThemeProvider>
   );
 };
