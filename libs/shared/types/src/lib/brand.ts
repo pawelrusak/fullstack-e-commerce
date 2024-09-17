f4b5a22986e97dfde32eb3504db4c7c7c7e1fcd6
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-17 10:55:51 +0200

    docs(types): add TODO comment to Brand type

--- libs/shared/types/src/lib/brand.ts
+++ libs/shared/types/src/lib/brand.ts
@@ -3,5 +3,6 @@ import type { Product } from './product';
 
 export type Brand = Id & {
   name: Product['brand'];
+  // TODO: Rename this to `productCount` or something similar
   count: number;
 };
