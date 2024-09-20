3b35eed29ecb090159d42771c90e2f45a053a933
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-20 22:37:26 +0200

    feat(ui): add screen reader label prop to Brand component

--- libs/shared/ui/src/lib/brand/brand.component.tsx
+++ libs/shared/ui/src/lib/brand/brand.component.tsx
@@ -1,17 +1,26 @@
 import { VisuallyHidden } from '@reach/visually-hidden';
 import { EN } from '@e-shop/i18n';
 import * as Styled from './Brand.styled';
+
 import type { StyledBrandSvgProps } from './Brand.styled';
 
+// TODO: Move this to a shared type library or directory
 type ClassNameAttribute = Pick<React.HTMLProps<HTMLElement>, 'className'>;
 
-export type BrandProps = StyledBrandSvgProps & ClassNameAttribute;
+export type BrandProps = StyledBrandSvgProps &
+  ClassNameAttribute & {
+    screenReaderLabel?: string;
+  };
 
-export function Brand({ className, ...rest }: BrandProps) {
+export function Brand({
+  className,
+  screenReaderLabel = EN.GLOBAL.BRAND_NAME,
+  ...rest
+}: BrandProps) {
   return (
     <Styled.Root className={className}>
       <Styled.BrandSvg {...rest} />
-      <VisuallyHidden>{EN.GLOBAL.BRAND_NAME}</VisuallyHidden>
+      <VisuallyHidden>{screenReaderLabel}</VisuallyHidden>
     </Styled.Root>
   );
 }
