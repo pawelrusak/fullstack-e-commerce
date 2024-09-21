441cc9cb6698530dd0204601b97cf79c42b1d21b
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-21 10:13:04 +0200

    feat(ui): add props to brand component

--- libs/shared/ui/src/lib/brand/Brand.styled.tsx
+++ libs/shared/ui/src/lib/brand/Brand.styled.tsx
@@ -4,6 +4,11 @@ import styled from 'styled-components';
 import { getComponentThemeToken as getToken } from '@e-shop/theme';
 import BrandSvgIcon from './brand.svg';
 
+import type {
+  BrandAccentThemeVariant,
+  BrandBaseThemeVariant,
+} from '@e-shop/theme/types';
+
 const { svgDot: svgDotToken, svgText: svgTextToken } = getToken('brand');
 
 export const Root = styled.span`
@@ -15,14 +20,18 @@ export const Root = styled.span`
 export type StyledBrandSvgProps = {
   textColor?: string;
   dotColor?: string;
+  accentTheme?: BrandAccentThemeVariant;
+  baseTheme?: BrandBaseThemeVariant;
 };
 
 export const BrandSvg = styled(BrandSvgIcon)<StyledBrandSvgProps>`
   [data-brand-text] {
-    fill: ${({ textColor }) => textColor ?? svgTextToken.primary.initial.fill};
+    fill: ${({ textColor, baseTheme = 'default' }) =>
+      textColor ?? svgTextToken[baseTheme].initial.fill};
   }
 
   [data-brand-dot] {
-    fill: ${({ dotColor }) => dotColor ?? svgDotToken.primary.initial.fill};
+    fill: ${({ dotColor, accentTheme = 'primary' }) =>
+      dotColor ?? svgDotToken[accentTheme].initial.fill};
   }
 `;
