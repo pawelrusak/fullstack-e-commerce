96a33fd77a61ea7a2f6c8bd4cfd730a01d48a537
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-21 10:16:10 +0200

    chore(ui): add arg types to stories of Brand component

--- libs/shared/ui/src/lib/brand/Brand.stories.tsx
+++ libs/shared/ui/src/lib/brand/Brand.stories.tsx
@@ -1,9 +1,30 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Brand } from './brand.component';
 
+import type { BrandProps } from './brand.component';
+
+type AccentThemes = BrandProps['accentTheme'][];
+type BaseThemes = BrandProps['baseTheme'][];
+
 export default {
   component: Brand,
   title: 'Brand',
+  argTypes: {
+    dotColor: {
+      control: 'color',
+    },
+    textColor: {
+      control: 'color',
+    },
+    accentTheme: {
+      control: 'select',
+      options: ['primary', 'secondary'] as AccentThemes,
+    },
+    baseTheme: {
+      control: 'select',
+      options: ['default', 'invert'] as BaseThemes,
+    },
+  },
 } satisfies Meta<typeof Brand>;
 
 type Story = StoryObj<typeof Brand>;
