64829c4f08a525de08f4e63a1dc868d2d5271f60
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-22 14:21:52 +0200

    chore(ui): add Storybook decorators for backgrounds

--- libs/shared/ui/.storybook/decorators.tsx
+++ libs/shared/ui/.storybook/decorators.tsx
@@ -1,6 +1,7 @@
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, css } from 'styled-components';
 import { GlobalStyle, AppThemeProvider, theme } from '@e-shop/theme';
-import { Decorator } from '@storybook/react';
+
+import type { Decorator } from '@storybook/react';
 
 export const GlobalStyleDecorator: Decorator = (Story) => {
   return (
@@ -22,7 +23,7 @@ export const AppThemeDecorator: Decorator = (Story) => {
 const SpacingHighlightContainer = styled.div`
   background-color: ${({ theme }) => theme.color.primaryContainer};
   border: 1px solid transparent;
-  margin-bottom: 12px;
+  margin: 1px;
 `;
 
 export const SpacingHighlightDecorator: Decorator = (Story) => {
@@ -31,6 +32,57 @@ export const SpacingHighlightDecorator: Decorator = (Story) => {
       <SpacingHighlightContainer>
         <Story />
       </SpacingHighlightContainer>
+    </ThemeProvider>
+  );
+};
+
+const baseSurfaceStyles = css`
+  padding: ${({ theme }) => `${theme.space[3]} ${theme.space[4]}`};
+  width: min-content;
+  border-radius: 10px;
+`;
+
+const NeutralSurfaceContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.surface};
+  ${baseSurfaceStyles};
+`;
+
+export const NeutralSurfaceDecorator: Decorator = (Story) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <NeutralSurfaceContainer>
+        <Story />
+      </NeutralSurfaceContainer>
+    </ThemeProvider>
+  );
+};
+
+const PrimarySurfaceContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.primary};
+  ${baseSurfaceStyles};
+`;
+
+export const PrimarySurfaceDecorator: Decorator = (Story) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <PrimarySurfaceContainer>
+        <Story />
+      </PrimarySurfaceContainer>
+    </ThemeProvider>
+  );
+};
+
+const SecondarySurfaceContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.secondary};
+  ${baseSurfaceStyles};
+`;
+
+export const SecondarySurfaceDecorator: Decorator = (Story) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <SecondarySurfaceContainer>
+        <Story />
+      </SecondarySurfaceContainer>
     </ThemeProvider>
   );
 };
