1a44feb46f6330328441cedf8f0a9e6e4253d04d
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-24 19:14:42 +0200

    refactor(ui): update token references in checkbox component

--- libs/shared/ui/src/lib/checkbox/checkbox.styled.tsx
+++ libs/shared/ui/src/lib/checkbox/checkbox.styled.tsx
@@ -11,27 +11,21 @@ export const Root = styled(Checkbox.Root)`
   width: 1.6rem;
   height: 1.6rem;
   border-radius: 0.2rem;
-  border: 1px solid
-    ${rootToken.variant.default.state.default.palette.borderColor};
-  background-color: ${rootToken.variant.default.state.default.palette
-    .backgroundColor};
+  border: 1px solid ${rootToken.default.initial.borderColor};
+  background-color: ${rootToken.default.initial.backgroundColor};
   align-items: center;
   justify-content: center;
   cursor: pointer;
 
   &[aria-checked='true'] {
-    background-color: ${rootToken.variant.default.state.checked.palette
-      .backgroundColor};
-    border-color: ${rootToken.variant.default.state.checked.palette
-      .borderColor};
+    border-color: ${rootToken.default.checked.borderColor};
+    background-color: ${rootToken.default.checked.backgroundColor};
   }
 
   &:hover,
   &:focus {
-    background-color: ${rootToken.variant.default.state.interact.palette
-      .backgroundColor};
-    border-color: ${rootToken.variant.default.state.interact.palette
-      .borderColor};
+    border-color: ${rootToken.default.interact.borderColor};
+    background-color: ${rootToken.default.interact.backgroundColor};
   }
 
   &:focus {
@@ -41,5 +35,5 @@ export const Root = styled(Checkbox.Root)`
 
 export const Indicator = styled(Checkbox.Indicator)`
   line-height: 0.8rem;
-  color: ${indicatorToken.base.palette.color};
+  color: ${indicatorToken._base.color};
 `;
