5a4b8a3ab85a46240372504987c7a6e498bfb671
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-10-13 19:25:51 +0200

    chore(ui): add size ArgTypes to IconButton story

--- libs/shared/ui/src/lib/icon-button/icon-button.stories.tsx
+++ libs/shared/ui/src/lib/icon-button/icon-button.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { ShareBigIcon } from '@e-shop/icons';
+import { ShareBigIcon, ShareIcon } from '@e-shop/icons';
 import { VisuallyHidden } from '@reach/visually-hidden';
 import { IconButton } from './icon-button.component';
-import { ICON_BUTTON_VARIANT } from './icon-button.utils';
+import { ICON_BUTTON_VARIANT, ICON_BUTTON_SIZE } from './icon-button.utils';
 
 export default {
   component: IconButton,
@@ -20,6 +20,14 @@ export default {
         ICON_BUTTON_VARIANT.OUTLINE_SECONDARY,
       ],
     },
+    size: {
+      control: 'select',
+      options: [
+        ICON_BUTTON_SIZE.SMALL,
+        ICON_BUTTON_SIZE.MEDIUM,
+        ICON_BUTTON_SIZE.LARGE,
+      ],
+    },
   },
 } satisfies Meta<typeof IconButton>;
 
@@ -28,6 +36,46 @@ type Story = StoryObj<typeof IconButton>;
 export const Default: Story = {
   args: {
     variant: undefined,
+    size: undefined,
+  },
+  render: (args) => (
+    <IconButton {...args}>
+      <ShareIcon />
+      <VisuallyHidden>Share</VisuallyHidden>
+    </IconButton>
+  ),
+};
+
+export const Small: Story = {
+  args: {
+    variant: undefined,
+    size: undefined,
+  },
+  render: (args) => (
+    <IconButton {...args}>
+      <ShareIcon />
+      <VisuallyHidden>Share</VisuallyHidden>
+    </IconButton>
+  ),
+};
+
+export const Medium: Story = {
+  args: {
+    variant: undefined,
+    size: undefined,
+  },
+  render: (args) => (
+    <IconButton {...args}>
+      <ShareIcon />
+      <VisuallyHidden>Share</VisuallyHidden>
+    </IconButton>
+  ),
+};
+
+export const Big: Story = {
+  args: {
+    variant: undefined,
+    size: ICON_BUTTON_SIZE.LARGE,
   },
   render: (args) => (
     <IconButton {...args}>
