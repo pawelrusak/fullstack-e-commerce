6151db7c5bfa9c9c1dd3bc2f352329e4fc79c780
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-10-22 21:09:43 +0200

    chore(ui): create level utility function for Heading component

---
+++ libs/shared/ui/src/lib/heading/heading.utils.ts
@@ -0,0 +1,41 @@
+import { css } from 'styled-components';
+import { getComponentThemeToken as getToken } from '@e-shop/theme';
+
+import type {
+  HeadingLevelModifier,
+  HeadingLevelNamespaceKey,
+  HeadingLevel as InternalHeadingLevel,
+} from '@e-shop/theme/types';
+import type { ObjectValues } from '@e-shop/types';
+
+const { root: rootToken } = getToken('heading');
+
+const HEADING_LEVEL_NAMESPACE: HeadingLevelNamespaceKey = 'level';
+
+export const HEADING_LEVEL = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+} satisfies Partial<Record<number, InternalHeadingLevel>>;
+
+export type HeadingLevel = ObjectValues<typeof HEADING_LEVEL>;
+
+function getLevelModifierKey(size: HeadingLevel): HeadingLevelModifier {
+  return `${HEADING_LEVEL_NAMESPACE}-${size}`;
+}
+
+export function getLevelStyle(level: HeadingLevel = HEADING_LEVEL[1]) {
+  const levelModifierKey = getLevelModifierKey(level);
+
+  return css`
+    font-size: ${rootToken[levelModifierKey].initial.fontSize};
+    line-height: ${rootToken[levelModifierKey].initial.lineHeight};
+    font-family: ${rootToken[levelModifierKey].initial.fontFamily};
+    font-weight: ${rootToken[levelModifierKey].initial.fontWeight};
+  `;
+}
