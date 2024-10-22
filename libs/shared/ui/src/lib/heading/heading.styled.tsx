4ca87665c7ede1e2adbefbb4025e923e6392668a
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-10-22 21:13:08 +0200

    refactor(ui): apply token references in Heading component

--- libs/shared/ui/src/lib/heading/heading.styled.tsx
+++ libs/shared/ui/src/lib/heading/heading.styled.tsx
@@ -1,69 +1,22 @@
 'use client';
 
-import { CSSProperties } from 'react';
 import styled from 'styled-components';
-import { theme } from '@e-shop/theme';
 import { margin } from 'styled-system';
+import { getComponentThemeToken as getToken } from '@e-shop/theme';
+import { getLevelStyle } from './heading.utils';
 
-const headingLevel = {
-  1: {
-    fontSize: '5.6rem',
-    fontWeight: theme.fontWeight.bold,
-    lineHeight: '6.8rem',
-  },
-  2: {
-    fontSize: '3.6rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '4.6rem',
-  },
-  3: {
-    fontSize: '2.4rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '3rem',
-  },
-  4: {
-    fontSize: '2rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '3rem',
-  },
-  5: {
-    fontSize: '2rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '3rem',
-  },
-  6: {
-    fontSize: '1.6rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '2.4rem',
-  },
-  7: {
-    fontSize: '1.6rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '2.4rem',
-  },
-  8: {
-    fontSize: '1.4rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '2rem',
-  },
-} satisfies Record<number, CSSProperties>;
+import type { HeadingLevel } from './heading.utils';
 
-const headingLevelExtra = {
-  ...headingLevel,
-  9: {
-    fontSize: '1.4rem',
-    fontWeight: theme.fontWeight.semiBold,
-    lineHeight: '2rem',
-  },
-} satisfies Record<number, CSSProperties>;
+const { root: rootToken } = getToken('heading');
 
 export type StyledHeadingProps = {
-  level?: keyof typeof headingLevel;
+  level?: HeadingLevel;
 };
 
 export const Heading = styled.h1<StyledHeadingProps>`
   display: block;
-  ${({ level = 1 }) => headingLevelExtra[level]}
+  color: ${rootToken._base.color};
+  ${({ level }) => getLevelStyle(level)};
 
   ${margin}
 `;
