5ecd1eae061280bde2782df39cc70b35d3878101
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-10-24 21:18:43 +0200

    refactor(ui): modify reexport in Button component

--- libs/shared/ui/src/lib/button/index.ts
+++ libs/shared/ui/src/lib/button/index.ts
@@ -1,4 +1,5 @@
-export { ButtonProps, Button } from './button.component';
+export { Button } from './button.component';
 export { BUTTON_VARIANT, BUTTON_COLOR_VARIANT } from './button.utils';
 
+export type { ButtonProps } from './button.component';
 export type { ButtonColorVariant, ButtonVariant } from './button.utils';
