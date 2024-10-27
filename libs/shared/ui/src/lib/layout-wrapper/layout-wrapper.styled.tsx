21eb2fdef78c7f96f25bb43e134b1ce5c0b04437
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-10-27 16:41:23 +0100

    refactor(ui): apply token references in LayoutWrappers component

--- libs/shared/ui/src/lib/layout-wrapper/layout-wrapper.styled.tsx
+++ libs/shared/ui/src/lib/layout-wrapper/layout-wrapper.styled.tsx
@@ -2,12 +2,17 @@
 
 import styled, { css } from 'styled-components';
 
+import { getComponentThemeToken as getToken } from '@e-shop/theme';
+
+const { root: rookToken } = getToken('layoutWrapper');
+
 export const layoutWrapperCss = css`
   padding-left: 1rem;
   padding-right: 1rem;
+  max-width: ${rookToken._base.maxWidth};
 
   @media screen and (min-width: ${({ theme }) => theme.screens.sm}) {
-    max-width: 156rem;
+    max-width: ${rookToken._base.smMaxWidth};
     padding-left: 2rem;
     padding-right: 2rem;
     margin: 0 auto;
