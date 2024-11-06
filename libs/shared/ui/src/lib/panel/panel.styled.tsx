f3be1ccf80a80e1e189f46120e6bc9e67c5b0df0
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-11-06 20:11:53 +0100

    refactor(ui): apply token references in Panel component

--- libs/shared/ui/src/lib/panel/panel.styled.tsx
+++ libs/shared/ui/src/lib/panel/panel.styled.tsx
@@ -1,35 +1,24 @@
 'use client';
 
-import styled, { css } from 'styled-components';
-import { theme } from '@e-shop/theme';
+import styled from 'styled-components';
+import { getTitleSizeStyle } from './panel.utils';
+import { getComponentThemeToken as getToken } from '@e-shop/theme';
+
+import type { InternalPanelTitleProps } from './panel.utils';
+
+const { root: rootToken, title: titleToken } = getToken('panel');
 
 export const SectionWrapper = styled.section`
-  background-color: ${({ theme }) => theme.color.secondBackground};
+  background-color: ${rootToken._base.backgroundColor};
   border-radius: 2.5rem;
   padding: 4rem;
 `;
 
-const titleSize = {
-  normal: {
-    fontSize: '2.4rem',
-    fontWeight: theme.fontWeight.semiBold,
-  },
-  small: {
-    fontSize: '2rem',
-    fontWeight: theme.fontWeight.bold,
-  },
-} satisfies Record<string, React.CSSProperties>;
-
-type StyledTitleProps = {
-  size?: keyof typeof titleSize;
-};
+type StyledTitleProps = InternalPanelTitleProps;
 
 export const Title = styled.h3<StyledTitleProps>`
-  font-family: ${({ theme }) => theme.fontFamily.montserrat};
+  color: ${titleToken._base.color};
+  display: block;
 
-  ${({ size }) => css`
-    font-size: ${titleSize[size || 'normal'].fontSize};
-    font-weight: ${titleSize[size || 'normal'].fontWeight};
-    line-height: 3rem;
-  `}
+  ${({ size }) => getTitleSizeStyle(size)};
 `;
