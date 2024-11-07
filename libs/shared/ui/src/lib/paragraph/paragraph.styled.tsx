697ea746ac3c506bc971d4aa6296731eeace8783
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-11-07 10:22:18 +0100

    refactor(ui): apply token references in Paragraph component

--- libs/shared/ui/src/lib/paragraph/paragraph.styled.tsx
+++ libs/shared/ui/src/lib/paragraph/paragraph.styled.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import styled from 'styled-components';
+import { getComponentThemeToken as getToken } from '@e-shop/theme';
+
+const { root: rootToken } = getToken('paragraph');
 
 export const RootParagraph = styled.p`
   text-align: justify;
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
-  font-size: 2rem;
-  line-height: 3rem;
+  color: ${rootToken._base.color};
+  font-weight: ${rootToken._base.fontWeight};
+  font-family: ${rootToken._base.fontFamily};
+  font-size: ${rootToken._base.fontSize};
+  line-height: ${rootToken._base.lineHeight};
 `;
