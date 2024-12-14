cdc09e543e9d54f8fd0b430145c902d5ea3724c4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-12-14 21:31:49 +0100

    docs(theme): add comments to device utility with size details

--- libs/shared/theme/src/lib/utils/device.utils.ts
+++ libs/shared/theme/src/lib/utils/device.utils.ts
@@ -9,9 +9,24 @@ function getMaxWidthFeatures(maxWidth: string) {
 }
 
 export const device = {
+  /**
+   * @device min-width: 640px
+   */
   sm: getMaxWidthFeatures(screens.sm),
+  /**
+   * @device min-width: 768px
+   */
   md: getMaxWidthFeatures(screens.md),
+  /**
+   * @device min-width: 1024px
+   */
   lg: getMaxWidthFeatures(screens.lg),
+  /**
+   * @device min-width: 1280px
+   */
   xl: getMaxWidthFeatures(screens.xl),
+  /**
+   * @device min-width: 1536px
+   */
   xxl: getMaxWidthFeatures(screens.xxl),
 } satisfies Partial<Device>;
