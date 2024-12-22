0cb451ac39adf4e64dc3964f28f5fa94e44bd8b4
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-12-22 22:35:35 +0100

    refactor(theme): rename internal function in device uitlity

--- libs/shared/theme/src/lib/utils/device.utils.ts
+++ libs/shared/theme/src/lib/utils/device.utils.ts
@@ -4,29 +4,29 @@ import type { AppScreens } from '../theme/tokens/types';
 
 type Device = AppScreens;
 
-function getMaxWidthFeatures(maxWidth: string) {
-  return `min-width: ${maxWidth}`;
+function getMinWidthFeatures(minWidth: string) {
+  return `min-width: ${minWidth}`;
 }
 
 export const device = {
   /**
    * @device min-width: 640px
    */
-  sm: getMaxWidthFeatures(screens.sm),
+  sm: getMinWidthFeatures(screens.sm),
   /**
    * @device min-width: 768px
    */
-  md: getMaxWidthFeatures(screens.md),
+  md: getMinWidthFeatures(screens.md),
   /**
    * @device min-width: 1024px
    */
-  lg: getMaxWidthFeatures(screens.lg),
+  lg: getMinWidthFeatures(screens.lg),
   /**
    * @device min-width: 1280px
    */
-  xl: getMaxWidthFeatures(screens.xl),
+  xl: getMinWidthFeatures(screens.xl),
   /**
    * @device min-width: 1536px
    */
-  xxl: getMaxWidthFeatures(screens.xxl),
+  xxl: getMinWidthFeatures(screens.xxl),
 } satisfies Partial<Device>;
