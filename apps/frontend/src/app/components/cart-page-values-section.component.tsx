80216b6dbcababf27ab32588271320a569ece913
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-01-07 18:27:31 +0100

    docs(frontend): add todo comment in CartPageValues

--- apps/frontend/src/app/components/cart-page-values-section.component.tsx
+++ apps/frontend/src/app/components/cart-page-values-section.component.tsx
@@ -5,13 +5,14 @@ import ValuesSection from './values-section.component';
 import { device } from '@e-shop/theme';
 import { LayoutWrapper } from '@e-shop/ui';
 
-// TODO: replace colors with theme references
+// TODO: replace this color with theme references
 const Wrapper = styled.div`
   border-top: 1px solid #cbcbcb;
   padding-top: clamp(2rem, 10vw + -3rem, 8rem);
   padding-bottom: clamp(2rem, 10vw + -3rem, 8rem);
 
   @media screen and (${device.lg}) {
+    /* TODO: replace this color with theme references  */
     border-bottom: 1px solid #cbcbcb;
   }
 `;
