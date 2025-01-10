f5ea0b1ab8cc1e78a861d3ec9556f24bf8402d7c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-01-10 19:18:26 +0100

    refactor(frontend): use device util for media queries in PageDivider

--- apps/frontend/src/app/components/page-divider.component.tsx
+++ apps/frontend/src/app/components/page-divider.component.tsx
@@ -2,11 +2,12 @@
 
 import styled from 'styled-components';
 import { Divider } from '@e-shop/ui';
+import { device } from '@e-shop/theme';
 
 const PageDivider = styled(Divider)`
   margin: 4rem 0 3.2rem;
 
-  @media screen and (min-width: ${({ theme }) => theme.screens.xl}) {
+  @media screen and (${device.xl}) {
     margin: 8rem 0 6.4rem;
   }
 `;
