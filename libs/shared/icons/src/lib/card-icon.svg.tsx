8caff53e2f5e4c87e0bc403db24d44e8cd818c03
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-01-20 17:43:57 +0100

    refactor(icons): rename cart icon component

--- libs/shared/icons/src/lib/card-icon.svg.tsx
+++ libs/shared/icons/src/lib/card-icon.svg.tsx
@@ -1,4 +1,4 @@
-export default function CardIcon(props: React.SVGProps<SVGSVGElement>) {
+export default function CartIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="20"
