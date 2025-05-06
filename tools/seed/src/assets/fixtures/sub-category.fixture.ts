0f7d58845a2dcc0bdb30db5682e45f41bb072031
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-05-06 18:44:22 +0200

    chore(seeder): add subCategory fixture

---
+++ tools/seed/src/assets/fixtures/sub-category.fixture.ts
@@ -0,0 +1,65 @@
+import { SubCategory, Modify } from '@e-shop/types';
+
+type SeederSubCategory = Modify<
+  SubCategory,
+  {
+    category: string;
+  }
+>;
+
+export default [
+  {
+    _id: '673292d0e7d109c007c4507f',
+    name: 'Phone',
+    slug: 'phone',
+    category: '673292d0e7d109c007c45077',
+  },
+  {
+    _id: '673292d0e7d109c007c45080',
+    name: 'Laptop',
+    slug: 'laptop',
+    category: '673292d0e7d109c007c45079',
+  },
+  {
+    _id: '673292d0e7d109c007c45081',
+    name: 'Camera',
+    slug: 'camera',
+    category: '673292d0e7d109c007c4507a',
+  },
+  {
+    _id: '673292d0e7d109c007c45082',
+    name: 'Television',
+    slug: 'television',
+    category: '673292d0e7d109c007c4507b',
+  },
+  {
+    _id: '673292d0e7d109c007c45083',
+    name: 'Watch',
+    slug: 'watch',
+    category: '673292d0e7d109c007c45077',
+  },
+  {
+    _id: '673292d0e7d109c007c45084',
+    name: 'Tablet',
+    slug: 'tablet',
+    category: '673292d0e7d109c007c45079',
+  },
+  {
+    _id: '673292d0e7d109c007c45085',
+    name: 'Home',
+    slug: 'home',
+    category: '673292d0e7d109c007c4507c',
+  },
+  {
+    _id: '673292d0e7d109c007c45086',
+    name: 'Audio',
+    slug: 'audio',
+    category: '673292d0e7d109c007c45078',
+  },
+  {
+    _id: '673292d0e7d109c007c45087',
+    name: 'Gaming',
+    slug: 'gaming',
+    category: '673292d0e7d109c007c4507d',
+  },
+] satisfies SeederSubCategory[];
