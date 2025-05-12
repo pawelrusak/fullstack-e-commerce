d3bfb4711273f4e0f7bdc1b8a61eb215210ff291
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-05-12 19:46:53 +0200

    chore(seeder): add categories fixture

---
+++ tools/seed/src/assets/fixtures/categories.fixtures.ts
@@ -0,0 +1,37 @@
+export default [
+  {
+    _id: '673292d0e7d109c007c4507c',
+    name: 'Home Appliances',
+    slug: 'home-appliances',
+  },
+  {
+    _id: '673292d0e7d109c007c45078',
+    name: 'Audio & Headphones',
+    slug: 'audio-headphones',
+  },
+  {
+    _id: '673292d0e7d109c007c4507a',
+    name: 'Cameras & Camcorders',
+    slug: 'cameras-camcorders',
+  },
+  {
+    _id: '673292d0e7d109c007c45077',
+    name: 'Mobile & Accessories',
+    slug: 'mobile-accessories',
+  },
+  {
+    _id: '673292d0e7d109c007c4507b',
+    name: 'TV & Home Theater',
+    slug: 'tv-home-theater',
+  },
+  {
+    _id: '673292d0e7d109c007c4507d',
+    name: 'Gaming Equipment',
+    slug: 'gaming-equipment',
+  },
+  {
+    _id: '673292d0e7d109c007c45079',
+    name: 'Computers & Tablets',
+    slug: 'computers-tablets',
+  },
+];
