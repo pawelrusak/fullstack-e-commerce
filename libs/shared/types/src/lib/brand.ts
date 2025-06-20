2460ec6bb904fbdc4174ce7558f51aa569333337
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2025-06-20 19:49:00 +0200

    docs(types): document Brand type

--- libs/shared/types/src/lib/brand.ts
+++ libs/shared/types/src/lib/brand.ts
@@ -1,6 +1,9 @@
 import type { Id } from './shared';
 import type { Product } from './product';
 
+/**
+ * Defines a brand entity in the shop's product catalog.
+ */
 export type Brand = Id & {
   name: Product['brand'];
   // TODO: Rename this to `productCount` or something similar
